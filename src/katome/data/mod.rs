880d3d7126574dc4b31a3c4312b35872fcb6c33c
Łukasz Neumann <fuine@riseup.net>
2016-04-19 20:17:44 +0200

    rcs

--- src/katome/data/mod.rs
+++ src/katome/data/mod.rs
@@ -3,13 +3,3 @@ pub mod input;
 pub mod types;
 pub mod read_slice;
 pub mod edges;
-mod table;
-pub mod map;
-
-trait Recover<Q: ?Sized> {
-    type Key;
-
-    fn get(&self, key: &Q) -> Option<&Self::Key>;
-    fn take(&mut self, key: &Q) -> Option<Self::Key>;
-    fn replace(&mut self, key: Self::Key) -> Option<Self::Key>;
-}
--- src/katome/lib.rs
+++ src/katome/lib.rs
@@ -1,14 +1,4 @@
-#![feature(heap_api)]
-#![feature(unique)]
-#![feature(core_intrinsics)]
-#![feature(oom)]
-#![feature(filling_drop)]
-#![feature(alloc)]
-#![feature(unsafe_no_drop_flag)]
-#![feature(dropck_parametricity)]
-
 extern crate pbr;
-
 
 pub mod data;
 // pub mod algorithms;
