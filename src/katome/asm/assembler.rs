f211cd0b59a5831f5ccfa0728abcc1746bb81243
Łukasz Neumann <fuine@riseup.net>
2016-04-22 20:12:51 +0200

    working naive rcs

--- src/katome/asm/assembler.rs
+++ src/katome/asm/assembler.rs
@@ -7,25 +7,21 @@ use std::cell::RefCell;
 
 // use ::pbr::{ProgressBar};
 
-// // this value is used only as a initiation dummy
-// pub static mut VECTOR_RC: VecRcPtr = 0 as VecRcPtr;
-// // this is only used to assure that the pointer to the Rc does not change throughout the program
-// static mut rc_dummy: VecRcPtr = 0 as VecRcPtr;
-
 pub fn make_it_happen(){
     let sequences: VecArc = Arc::new(RefCell::new(Vec::new()));
-    // unsafe{
-        // VECTOR_RC = &sequences as VecRcPtr;
-    // }
     let mut graph: Graph = Graph::new();
     // read_sequences("***REMOVED***".to_string(),
-    read_sequences("***REMOVED***".to_string(),
     // read_sequences("***REMOVED***".to_string(),
     // read_sequences("***REMOVED***".to_string(),
+    // read_sequences("***REMOVED***".to_string(),
+    read_sequences("./data/test2.txt".to_string(),
                    sequences.clone(), &mut graph);
-    print!("{}", sequences.borrow().len());
-    // unsafe{
-        // rc_dummy = &sequences as VecRcPtr;
-    // }
-    // assert!(unsafe{VECTOR_RC == rc_dummy}, "Pointer to Rc changed!");
+    println!("Number of unique sequences: {}", sequences.borrow().len());
+    for (key, val) in graph.iter() {
+    // for val in graph.values() {
+        println!("{}: {:?}", key.name(), val.outgoing.iter().fold(Vec::new() as Vec<u64>, |mut vec, ref x| {vec.push(x.1); vec}));
+        // println!("{:?}", val.outgoing);
+    }
+
+
 }
