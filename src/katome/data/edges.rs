d4fa555a0136cb8db0cf310fffd87700578534b3
Łukasz Neumann <linfuinea@gmail.com>
2016-04-26 12:58:10 +0200

    input and output vertices are now being found during creation of the graph

--- src/katome/data/edges.rs
+++ src/katome/data/edges.rs
@@ -4,10 +4,10 @@ use data::types::{VertexId};
 #[derive(Clone)]
 pub struct Edges{
     pub outgoing: Vec<(VertexId, u64)>, // data is aligned to ptr in tuple
-    pub in_size: u64,                        // data is aligned to 8 bytes in this struct
+    pub in_num: u32,                        // data is aligned to 8 bytes in this struct
+    pub out_num: u32
     // pub weights: Vec<u32>,
     // pub in_size: u32,
-    // pub out_size: u32
     // pub in_vertices: Vec<ReadSlice>
 }
 
@@ -15,18 +15,18 @@ impl Edges {
     pub fn new(to: VertexId) -> Edges {
         Edges {
             outgoing: vec![(to, 1)],
+            in_num: 0,
+            out_num: 1,
             // weights: vec![1],
-            in_size: 0,
-            // out_size: 1,
             // in_vertices: vec![]
         }
     }
     pub fn empty() -> Edges {
         Edges {
             outgoing: vec![],
+            in_num: 0,
+            out_num: 0,
             // weights: vec![1],
-            in_size: 1,
-            // out_size: 1,
             // in_vertices: vec![]
         }
     }
